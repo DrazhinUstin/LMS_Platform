c03eac807342a5d7be7e75dcba08c11c18fd177d
Drazhin Ustin <zmagar91@gmail.com>
2025-11-17 16:08:06 +0300

    Add course progress

---
+++ app/customer/courses/[id]/course-progress.tsx
@@ -0,0 +1,27 @@
+import { Progress } from '@/app/components/ui/progress';
+import { UserCourseTypeWithInclude } from '@/app/data/course/get-user-course';
+
+export default function CourseProgress({ course }: { course: UserCourseTypeWithInclude }) {
+  const totalLessons = course.chapters.reduce((acc, chapter) => acc + chapter._count.lessons, 0);
+
+  const totalCompletedLessons = course.chapters.reduce((acc, chapter) => {
+    let chapterLessonsCompleted = 0;
+
+    chapter.lessons.forEach(
+      (lesson) => lesson.userProgresses[0]?.isCompleted && chapterLessonsCompleted++
+    );
+
+    return acc + chapterLessonsCompleted;
+  }, 0);
+
+  return (
+    <div className="space-y-2">
+      <h3 className="truncate text-xl font-semibold">{course.title}</h3>
+      <Progress value={(totalCompletedLessons / totalLessons) * 100} />
+      <p className="text-muted-foreground flex justify-between gap-x-1 text-xs">
+        <span>Completed lessons: {totalCompletedLessons}</span>
+        <span>Total lessons: {totalLessons}</span>
+      </p>
+    </div>
+  );
+}
--- app/customer/courses/[id]/layout.tsx
+++ app/customer/courses/[id]/layout.tsx
@@ -3,6 +3,7 @@ import CourseStructure from './course-structure';
 import { getUserCourse } from '@/app/data/course/get-user-course';
 import { auth } from '@/app/lib/auth';
 import { headers } from 'next/headers';
+import CourseProgress from './course-progress';
 
 export default async function Layout({
   children,
@@ -28,7 +29,8 @@ export default async function Layout({
   return (
     <div className="grid gap-8 lg:grid-cols-[1fr_320px] lg:items-start">
       <div>{children}</div>
-      <aside className="lg:sticky lg:top-8">
+      <aside className="space-y-4 lg:sticky lg:top-8">
+        <CourseProgress course={course} />
         <CourseStructure course={course} />
       </aside>
     </div>
